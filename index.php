66a982388e0d3fcf0afc897e06ab687ac85cf436
Jarvl <ajarvis7771@gmail.com>
2014-04-06 11:15:36 -0400

    change map onclick

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script src="js/functions.js"></script>
     <script>
-    google.maps.event.addDomListener(document.getElementById('showButton'), 'onclick', function(){ initialize(); });
+    google.maps.event.addDomListener(document.getElementById('showButton'), 'onclick', initialize);
     </script>
 </head>
 <body>
