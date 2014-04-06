a67b42c40e64cc072520fbe705e629afa93def42
dotContent <dotmaners@gmail.com>
2014-04-06 12:05:31 -0400

    Hi

--- location.php
+++ location.php
@@ -40,8 +40,8 @@ if(isset($_POST['latitude']) && isset($_POST['longitude'])) {
     </div>
 
     <script>
-        var myLatitude = navigator.geolocation.coords.latitude;
-        var myLongitude = navigator.geolocation.coords.longitude;
+        var myLatitude = position.coords.latitude;
+        var myLongitude = position.coords.longitude;
 
         var myLatLng = new google.maps.LatLng(myLatitude, myLongitude);
         var theirLatlng = new google.maps.LatLng(myLatitude, myLongitude);
