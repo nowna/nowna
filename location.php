61bffa430ddb40d9c3aef2f0e07f387ec6364a94
Jarvl <ajarvis7771@gmail.com>
2014-04-05 22:45:44 -0400

    edited location, got ajax working (?)

--- location.php
+++ location.php
@@ -1,3 +1,11 @@
 <?php
 
+if(isset($_POST['latitude']) && isset($_POST['longitude'])) {
+	$latitude = $_POST['latitude'];
+	$longitude = $_POST['longitude'];
+}
+else {
+	echo "<script>alert('Something went wrong with the AJAx request!')";
+}
+
 ?>
