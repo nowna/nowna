863a8bfc1b8baea201c9559e265c06d4fc2b151c
Jarvl <ajarvis7771@gmail.com>
2014-04-06 05:37:01 -0400

    ugh

--- js/geo.js
+++ js/geo.js
@@ -35,8 +35,9 @@ function showPosition(position)
     })
     .done(function(data) {
         $('#tweet_box').html(data);
-        $('#refresh').html("<button href="javascript:getLocation()" class="btn btn-small btn-default fa fa-refresh" id="tabHeader_1">Refresh</button>");
     }).fail(function() {
         $('#tweet_box').html('The ajax request failed or we ran out of requests!');
     });
+
+    $('#refresh').html("<button href="javascript:getLocation()" class="btn btn-small btn-default fa fa-refresh" id="tabHeader_1">Refresh</button>");
 }
